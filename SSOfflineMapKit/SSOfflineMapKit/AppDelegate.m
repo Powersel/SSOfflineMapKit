90b1403558fe63ebd4e661dc217cfc56b198d0d6
Sergiy Shevchuk <sps@anadeainc.com>
2016-08-26 17:32:01 +0300

    AppDelegate -> added initialisation of the SSMapVC

--- SSOfflineMapKit/SSOfflineMapKit/AppDelegate.m
+++ SSOfflineMapKit/SSOfflineMapKit/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "SSMapViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,12 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    SSMapViewController *mapViewController = [[SSMapViewController alloc] initWithNibName:nil bundle:nil];
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = mapViewController;
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
