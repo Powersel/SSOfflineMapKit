60f6ba82143859b2b6c2422ea953a7ad48628d87
Sergiy Shevchuk <sps@anadeainc.com>
2016-08-26 16:49:35 +0300

    SSMapViewController -> created

---
+++ SSOfflineMapKit/SSOfflineMapKit/SSMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSMapViewController.h
+//  SSOfflineMapKit
+//
+//  Created by Sergiy Shevchuk on 8/26/16.
+//  Copyright © 2016 Sergiy Shevchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSMapViewController : UIViewController
+
+@end
