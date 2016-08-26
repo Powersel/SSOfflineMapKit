5e31b051e0e7d5f870d1092238db286ed950696f
Sergiy Shevchuk <sps@anadeainc.com>
2016-08-26 15:37:52 +0300

    Initial commit

---
+++ SSOfflineMapKit/SSOfflineMapKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SSOfflineMapKit
+//
+//  Created by Sergiy Shevchuk on 8/26/16.
+//  Copyright © 2016 Sergiy Shevchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SSOfflineMapKit/SSOfflineMapKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSOfflineMapKit
+//
+//  Created by Sergiy Shevchuk on 8/26/16.
+//  Copyright © 2016 Sergiy Shevchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
