08005e968d62b7f2b53df9896cd9c90f0df881c5
Sergiy Shevchuk <sps@anadeainc.com>
2016-08-30 16:30:02 +0300

    SSTileOverlay -> added mechanism of tiles exchange

--- SSOfflineMapKit/SSOfflineMapKit/SSMapViewController.m
+++ SSOfflineMapKit/SSOfflineMapKit/SSMapViewController.m
@@ -4,7 +4,7 @@
 #import "SSMapViewController.h"
 #import "GridTileOverlay.h"
 #import "GridTileOverlayRenderer.h"
-
+#import "SSZipArchive.h"
 #import "SSTileOverlay.h"
 #import "SSTileOverlayRender.h"
 
@@ -12,11 +12,25 @@
 @property (nonatomic, strong) MKTileOverlay *tileOverlay;
 @property (strong, nonatomic) MKTileOverlay *gridOverlay;
 
-- (void)reloadTileOverlay;
+@property (nonatomic, strong) NSString *tilesFolderPath;
+
+//- (void)reloadTileOverlay;
+- (void)unzipArchive;
 
 @end
 
 @implementation SSMapViewController
+
+#pragma mark - Initialisation and Dealoccation
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        [self unzipArchive];
+    }
+    
+    return self;
+}
 
 #pragma mark - UIViewController Life Cycle
 
@@ -25,25 +39,26 @@
     
     MKMapView *mapView = self.mapView;
     
-    CLLocationDegrees latitude = -33.6168364247;
-    CLLocationDegrees longitude = 151.1298788992;
+    CLLocationDegrees latitude = -33.4316798442;
+    CLLocationDegrees longitude = 151.2765980607;
     CLLocationCoordinate2D center = CLLocationCoordinate2DMake(latitude, longitude);
-    MKCoordinateSpan coordinateSpan = MKCoordinateSpanMake(0.2, 0.2);
+    MKCoordinateSpan coordinateSpan = MKCoordinateSpanMake(0.02, 0.02);
     MKCoordinateRegion region = MKCoordinateRegionMake(center, coordinateSpan);
-    [mapView setRegion:region];
+    [mapView setRegion:region animated:YES];
     
 //    // Coordinats grid
-    self.gridOverlay = [[GridTileOverlay alloc] init];
-    self.gridOverlay.canReplaceMapContent = NO;
-    [mapView addOverlay:self.gridOverlay level:MKOverlayLevelAboveLabels];
+//    self.gridOverlay = [[GridTileOverlay alloc] init];
+//    self.gridOverlay.canReplaceMapContent = NO;
+//    [mapView addOverlay:self.gridOverlay level:MKOverlayLevelAboveLabels];
 //
-//    self.tileOverlay = [[SSTileOverlay alloc] init];
-//    self.tileOverlay.canReplaceMapContent = YES;
+    self.tileOverlay = [[SSTileOverlay alloc] init];
+    self.tileOverlay.canReplaceMapContent = YES;
+    [self.mapView addOverlay:self.tileOverlay level:MKOverlayLevelAboveLabels];
 //    [self.mapView insertOverlay:self.tileOverlay belowOverlay:self.gridOverlay];
     
     mapView.delegate = self;
     
-    [self reloadTileOverlay];
+//    [self reloadTileOverlay];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -52,28 +67,35 @@
 
 #pragma mark - MKMapViewDelegate Methods
 
-//- (void)mapViewWillStartLoadingMap:(MKMapView *)mapView {
-//    NSArray *tiles = mapView.overlays;
-//    
-//}
-
 - (MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id <MKOverlay>)overlay {
-    MKTileOverlay *customOverlay = (MKTileOverlay *)overlay;
+    if ([overlay isKindOfClass:[MKTileOverlay class]]) {
+        return [[MKTileOverlayRenderer alloc] initWithTileOverlay:overlay];
+    }
     
-//    MKTileOverlayRenderer *renderedTile = [[SSTileOverlayRender alloc] initWithTileOldOverlay:overlay];;
-    MKTileOverlayRenderer *renderer = [[GridTileOverlayRenderer alloc] initWithTileOverlay:overlay];;
-    
-    return renderer;
+    return nil;
 }
 
 #pragma mark - Private
 
-- (void)reloadTileOverlay {
-    NSString *baseURL = [[[NSBundle mainBundle] bundleURL] absoluteString];
-    NSString *urlTemplate = [baseURL stringByAppendingString:@"{z}_{x}_{y}.jpg"];
-    
-    self.tileOverlay = [[MKTileOverlay alloc] initWithURLTemplate:urlTemplate];
-    self.tileOverlay.canReplaceMapContent = YES;
-    [self.mapView insertOverlay:self.tileOverlay belowOverlay:self.gridOverlay];}
+//- (void)reloadTileOverlay {
+//    NSString *baseURL = [[[NSBundle mainBundle] bundleURL] absoluteString];
+//    NSString *urlTemplate = [baseURL stringByAppendingString:@"{z}_{x}_{y}.jpg"];
+//    
+//    self.tileOverlay = [[MKTileOverlay alloc] initWithURLTemplate:urlTemplate];
+//    self.tileOverlay.canReplaceMapContent = YES;
+//    [self.mapView insertOverlay:self.tileOverlay belowOverlay:self.gridOverlay];
+//}
+
+- (void)unzipArchive {
+    NSString *archivePath = [[NSBundle mainBundle] pathForResource:@"ww_nsw-bwnp-gtpxx_maps" ofType:@"zip"];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    self.tilesFolderPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/nsw-bwnp-gtpxx"];
+    if (![fileManager fileExistsAtPath:self.tilesFolderPath]) {
+        NSError *error = nil;
+        [fileManager createDirectoryAtPath:self.tilesFolderPath withIntermediateDirectories:NO attributes:nil error:&error];
+        NSLog(@"%@", error);
+    }
+    [SSZipArchive unzipFileAtPath:archivePath toDestination:self.tilesFolderPath];
+}
 
 @end
--- SSOfflineMapKit/SSOfflineMapKit/SSTileOverlay.m
+++ SSOfflineMapKit/SSOfflineMapKit/SSTileOverlay.m
@@ -27,26 +27,42 @@
 
 - (void)loadTileAtPath:(MKTileOverlayPath)path
                 result:(void (^)(NSData * _Nullable, NSError * _Nullable))result {
-    NSLog(@"Loading tile x/y/z: %ld/%ld/%ld",(long)path.x,(long)path.y,(long)path.z);
+    
+  
+    
+    NSLog(@"Loading tile in SSTileOverlay z/x/y: %ld/%ld/%ld", (long)path.z, (long)path.x, (long)path.y);
+    NSString *tileName = [NSString stringWithFormat:@"%ld_%ld_%ld.jpg",(long)path.z, (long)path.x,(long)path.y];
+    NSString *tilesFolder = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/nsw-bwnp-gtpxx"];
+    NSString *tilePath = [tilesFolder stringByAppendingPathComponent:tileName];
+    
+    NSData *tileData;
+    BOOL isTileExist = [[NSFileManager defaultManager] fileExistsAtPath:tilePath];
+    NSLog(@"Is tile exist: %@", @(isTileExist));
     
     if (!result) {
         return;
     }
     
+    if (isTileExist) {
+        UIImage *tileImage = [[UIImage alloc] initWithContentsOfFile:tilePath];
+//        UIImage *tileImage = [UIImage imageWithContentsOfFile:tilePath];
+        tileData = UIImageJPEGRepresentation(tileImage, 1);
+    } else {
+        CGSize sz = self.tileSize;
+        CGRect rect = CGRectMake(0, 0, sz.width, sz.height);
+        UIGraphicsBeginImageContext(sz);
+        CGContextRef ctx = UIGraphicsGetCurrentContext();
+        [[UIColor blackColor] setStroke];
+        CGContextSetLineWidth(ctx, 1.0);
+        CGContextStrokeRect(ctx, CGRectMake(0, 0, sz.width, sz.height));
+        
+        NSString *text = [NSString stringWithFormat:@"X=%ld\nY=%ld\nZ=%ld",(long)path.x,(long)path.y,(long)path.z];
+        [text drawInRect:rect withAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:20.0],
+                                               NSForegroundColorAttributeName:[UIColor blackColor]}];
+        UIImage *tileImage = UIGraphicsGetImageFromCurrentImageContext();
+        tileData = UIImagePNGRepresentation(tileImage);
+    }
     
-    CGSize sz = self.tileSize;
-    CGRect rect = CGRectMake(0, 0, sz.width, sz.height);
-    UIGraphicsBeginImageContext(sz);
-    CGContextRef ctx = UIGraphicsGetCurrentContext();
-    [[UIColor blackColor] setStroke];
-    CGContextSetLineWidth(ctx, 1.0);
-    CGContextStrokeRect(ctx, CGRectMake(0, 0, sz.width, sz.height));
-    NSString *text = [NSString stringWithFormat:@"X=%ld\nY=%ld\nZ=%ld",(long)path.x,(long)path.y,(long)path.z];
-    [text drawInRect:rect withAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:20.0],
-                                           NSForegroundColorAttributeName:[UIColor blackColor]}];
-    UIImage *tileImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    NSData *tileData = UIImagePNGRepresentation(tileImage);
     result(tileData,nil);
 }
 
