883df96f0b83e1fce831d0182f32226f97657e90
Sergiy Shevchuk <sps@anadeainc.com>
2016-08-31 10:17:09 +0300

    SSAnnotationView -> created-added

---
+++ SSOfflineMapKit/SSOfflineMapKit/SSAnnotationView.h
@@ -0,0 +1,13 @@
+//
+//  SSAnnotationView.h
+//  SSOfflineMapKit
+//
+//  Created by Sergiy Shevchuk on 8/30/16.
+//  Copyright © 2016 Sergiy Shevchuk. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface SSAnnotationView : MKAnnotationView
+
+@end
