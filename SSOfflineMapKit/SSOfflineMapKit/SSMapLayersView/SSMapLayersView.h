16739af3704ea16a37a515a1b8ba9ba58b4d13e2
Sergiy Shevchuk <sps@anadeainc.com>
2016-09-07 15:41:31 +0300

    SSMapLayersView -> created

--- SSOfflineMapKit/SSOfflineMapKit/SSMapLayersView/SSMapLayersView.h
+++ SSOfflineMapKit/SSOfflineMapKit/SSMapLayersView/SSMapLayersView.h
@@ -7,7 +7,30 @@
 //
 
 #import "HYIBDesignableView.h"
+#import "WWBlockTypeDef.h"
+
+typedef NS_ENUM(NSUInteger, WWLayerButtons) {
+    WWPOIButton = 0,
+    WWMainWPButton,
+    WWMainTrackButton,
+    WWSidetripsButton,
+    WWAlternatesButton,
+    WWImagesButton
+};
 
 @interface SSMapLayersView : HYIBDesignableView
+@property (strong, nonatomic) IBOutlet UIButton *POIButton;
+@property (strong, nonatomic) IBOutlet UIButton *mainWPButton;
+@property (strong, nonatomic) IBOutlet UIButton *mainTrackButton;
+@property (strong, nonatomic) IBOutlet UIButton *sidetripsButton;
+@property (strong, nonatomic) IBOutlet UIButton *alternatesButton;
+@property (strong, nonatomic) IBOutlet UIButton *imagesButton;
+
+@property (nonatomic, strong) IBOutletCollection(UIButton) NSArray *buttons;
+
+- (IBAction)buttonClicked:(id)sender;
+
+- (void)setButtonsStateWith:(NSDictionary *)state dictionaryCompletion:(WWDictionaryBlock)completionBlock;
+- (void)setButtonsStateWith:(NSArray *)state completion:(WWResultBlock)completionBlock;
 
 @end
--- SSOfflineMapKit/SSOfflineMapKit/SSMapLayersView/SSMapLayersView.m
+++ SSOfflineMapKit/SSOfflineMapKit/SSMapLayersView/SSMapLayersView.m
@@ -1,13 +1,39 @@
-//
-//  SSMapLayersView.m
-//  SSOfflineMapKit
-//
 //  Created by Sergiy Shevchuk on 9/7/16.
 //  Copyright © 2016 Sergiy Shevchuk. All rights reserved.
-//
 
 #import "SSMapLayersView.h"
 
+@interface SSMapLayersView ()
+@property (nonatomic, copy) WWDictionaryBlock   dictionaryCompletion;
+@property (nonatomic, copy) WWResultBlock       resultCompletion;
+
+@property (nonatomic, strong) NSDictionary  *buttonsState;
+
+@end
+
 @implementation SSMapLayersView
+
+- (IBAction)buttonClicked:(id)sender {
+    if (self.resultCompletion) {
+        self.resultCompletion(@([sender tag]));
+    }
+}
+
+- (void)setButtonsStateWith:(NSDictionary *)state dictionaryCompletion:(WWDictionaryBlock)completionBlock {
+    
+}
+
+- (void)setButtonsStateWith:(NSArray *)state completion:(WWResultBlock)completionBlock {
+    if (completionBlock) {
+        self.resultCompletion = completionBlock;
+    }
+    for (NSInteger index =0; index >= state.count; index++) {
+        NSNumber *buttonState = state[index];
+        BOOL isButtonClicked = buttonState.boolValue;
+        if (isButtonClicked) {
+            
+        }
+    }
+}
 
 @end
