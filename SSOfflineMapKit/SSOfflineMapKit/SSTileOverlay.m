a7e3e0ffd5a085d921d3458973dadba5a660c1e4
Sergiy Shevchuk <sergiyshevchuk@Sergiys-MacBook-Pro.local>
2016-09-15 13:32:16 +0300

    SSTileOverlay -> fixed memory leaks.

--- SSOfflineMapKit/SSOfflineMapKit/SSTileOverlay.m
+++ SSOfflineMapKit/SSOfflineMapKit/SSTileOverlay.m
@@ -9,56 +9,45 @@
 
 @implementation SSTileOverlay
 
-//- (NSURL *)URLForTilePath:(MKTileOverlayPath)path {
-//    NSString *tileKey = [[NSString alloc] initWithFormat:@"%ld_%ld_%ld.jpg", (long)path.x, (long)path.y, (long)path.z];
-//    
-//    return [NSURL URLWithString:tileKey];
-//}
-//
-//
-//// NEW overloaded methods
-//
-//- (instancetype)initWithURLTemplate:(nullable NSString *)URLTemplate {
-//    self = [super initWithURLTemplate:URLTemplate];
-//    
-//    
-//    return self;
-//}
-
 - (void)loadTileAtPath:(MKTileOverlayPath)path
-                result:(void (^)(NSData * _Nullable, NSError * _Nullable))result {
+                result:(void (^)(NSData  *_Nullable, NSError  *_Nullable))result
+{
     NSString *tileName = [NSString stringWithFormat:@"%ld_%ld_%ld.jpg",(long)path.z, (long)path.x,(long)path.y];
     NSString *tilesFolder = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/nsw-bmnp-sft"];
     NSString *tilePath = [tilesFolder stringByAppendingPathComponent:tileName];
-    NSData *tileData = nil;
     BOOL isTileExist = [[NSFileManager defaultManager] fileExistsAtPath:tilePath];
     
     if (!result) {
         return;
     }
     
-    if (isTileExist) {
-        UIImage *tileImage = [[UIImage alloc] initWithContentsOfFile:tilePath];
-//        UIImage *tileImage = [UIImage imageWithContentsOfFile:tilePath];
-        tileData = UIImageJPEGRepresentation(tileImage, 1);
-    } else {
-        CGSize sz = self.tileSize;
-        CGRect rect = CGRectMake(0, 0, sz.width, sz.height);
-        UIGraphicsBeginImageContext(sz);
-        CGContextRef ctx = UIGraphicsGetCurrentContext();
-        [[UIColor blackColor] setStroke];
-        CGContextSetLineWidth(ctx, 1.0);
-        CGContextStrokeRect(ctx, CGRectMake(0, 0, sz.width, sz.height));
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0), ^{
+        NSData *tileData = nil;
         
-        NSString *text = [NSString stringWithFormat:@"X=%ld\nY=%ld\nZ=%ld",(long)path.x,(long)path.y,(long)path.z];
-        [text drawInRect:rect withAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:20.0],
-                                               NSForegroundColorAttributeName:[UIColor blackColor]}];
-        UIImage *tileImage = UIGraphicsGetImageFromCurrentImageContext();
-        tileData = UIImagePNGRepresentation(tileImage);
-    }
-    
-    result(tileData,nil);
+        if (isTileExist) {
+            tileData = [NSData dataWithContentsOfFile:tilePath
+                                              options:NSDataReadingUncached
+                                                error:NULL];
+        } else {
+            CGSize sz = self.tileSize;
+            CGRect rect = CGRectMake(0, 0, sz.width, sz.height);
+            UIGraphicsBeginImageContext(sz);
+            CGContextRef ctx = UIGraphicsGetCurrentContext();
+            [[UIColor blackColor] setStroke];
+            CGContextSetLineWidth(ctx, 1.0);
+            CGContextStrokeRect(ctx, CGRectMake(0, 0, sz.width, sz.height));
+            
+            NSString *text = [NSString stringWithFormat:@"X=%ld\nY=%ld\nZ=%ld",(long)path.x,(long)path.y,(long)path.z];
+            [text drawInRect:rect withAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:20.0],
+                                                   NSForegroundColorAttributeName:[UIColor blackColor]}];
+            UIImage *tileImage = UIGraphicsGetImageFromCurrentImageContext();
+            tileData = UIImagePNGRepresentation(tileImage);
+            
+            UIGraphicsEndImageContext();
+        }
+        
+        result(tileData,nil);
+    });
 }
-
 
 @end
